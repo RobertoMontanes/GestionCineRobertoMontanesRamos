3e499fa5ca33b126eb8afabeb049c4d697bae3fb
Roberto Montañés Ramos <rmr15092004@gmail.com>
2025-05-14 13:21:25 +0200

    base de datos tmb

--- RobertoMontanesRamos/src/main/java/com/salesianostriana/dam/controller/EntradaController.java
+++ RobertoMontanesRamos/src/main/java/com/salesianostriana/dam/controller/EntradaController.java
@@ -5,6 +5,8 @@ import com.salesianostriana.dam.model.Sala;
 import com.salesianostriana.dam.service.EntradaService;
 import com.salesianostriana.dam.service.SalaService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +17,10 @@ import java.time.LocalDateTime;
 @RequestMapping("/admin/entrada")
 @RequiredArgsConstructor
 public class EntradaController {
-
-	private final EntradaService entradaService;
+	
+	@Autowired
+	private EntradaService entradaService;
+	@Autowired
 	private final SalaService salaService;
 
 	@GetMapping("/")
