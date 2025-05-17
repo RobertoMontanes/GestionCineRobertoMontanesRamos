25efb4fc9dcea26ff1a012a9c1d7b2e3ea196139
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-05-17 22:23:00 +0200

    pagina error perfe

--- RobertoMontanesRamos/src/main/java/com/salesianostriana/dam/repository/SalaRepository.java
+++ RobertoMontanesRamos/src/main/java/com/salesianostriana/dam/repository/SalaRepository.java
@@ -1,7 +1,16 @@
 package com.salesianostriana.dam.repository;
 
 import com.salesianostriana.dam.model.Sala;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SalaRepository extends JpaRepository<Sala, Long> {
+ 
+ List<Sala> findByNombreContainingIgnoreCase(String nombre);
+ 
+
+ @Query("SELECT s FROM Sala s WHERE LOWER(s.nombre) LIKE LOWER(concat('%', :query, '%'))")
+ List<Sala> buscarSalas(@Param("query") String query);
 }
